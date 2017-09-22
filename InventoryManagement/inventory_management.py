0c71f100e1104699818ba99901165c682dc95bb1
Alexei Bushuev <alexeib.2017@gmail.com>
2017-09-22 13:06:01 +0300

    add OrderManager

--- InventoryManagement/inventory_management.py
+++ InventoryManagement/inventory_management.py
@@ -10,7 +10,6 @@ class WareHouse(object):
         :param sku: The SKU to increment from.
         :param amount: The amount to increment.
         """
-
         if sku not in self.inventory:
             self.inventory[sku] = 0
 
@@ -34,12 +33,20 @@ class WareHouse(object):
         self.inventory[sku] -= amount
         return True
 
+    def get_amount(self,sku):
+        if sku not in self.inventory:
+            return 0
+        return self.inventory[sku]
+
+    def get_name(self):
+        return self.name
+
 
 class Order(object):
     def __init__(self, sku, amount):
         """
         Create order to be fulfilled and shipped
-        :param sku: SKU to be ordered (todo: list of SKU to one order)
+        :param sku: SKU to be ordered (todo: list of SKU in one order)
         :param amount: amount of SKU
         """
         self.acknowledged = False  
@@ -47,9 +54,21 @@ class Order(object):
         self.shipped = False
         self.rejected = False
         self.tracking_number = ""
-        self.sku = ''
-        self.amount = 0
+        self.sku = sku
+        self.amount = amount
         self.warehouses = {}
+
+    def __str__(self):
+        tags = []
+        if self.acknowledged:
+            tags.append('acknowledged')
+        if self.canceled:
+            tags.append('canceled')
+        if self.shipped:
+            tags.append('shipped')
+        if self.rejected:
+            tags.append('rejected')
+        return '<Order: %s[%i] %s>' % (self.sku, self.amount, ','.join(tags))
 
 
 class OrderManager(object):
@@ -66,40 +85,56 @@ class OrderManager(object):
         ship the order. However, the we do not know which warehouse 
         it will be shipped from.
 
-        We only acknowledge if we have enough inventory. Fix the 
-        code below to only acknowledge when inventory is available 
-        in any warehouse. Perform any inventory management you need
-        to do.
+        We only acknowledge if we have enough inventory.
         """
-        if True:
+        total_amount = 0
+        for warehouse in self.warehouses:
+            total_amount += warehouse.get_amount(order.sku)
+        if total_amount >= order.amount :
             order.acknowledged = True
             return True
         else:
             order.rejected = True
             return False
 
-    def pack_order(self, order):
-        """
-        Pack the order on warehouse(s)
-        :param order:
-        :return:
-        """
-        return True
-
-    def ship_order(self, order, tracking_number, warehouse_name):
+    def ship_order(self, order, tracking_number):
         """
         At this point, the order has been shipped and seller has 
                 determined which warehouse they will be shipping from.
         :param order: the order to ship
         :param tracking_number: Tracking Number of the shipment
-        :param warehouse_name: Name of WareHouse it was Shipped From.
         :return: True if Shipped False otherwise.
-
-        Perform any inventory management you need to do here.
         """
-        order.shipped = True
-        order.tracking_number = tracking_number
-        return order.shipped
+
+        # Optimization: find single warehouse with enough amount
+        for warehouse in self.warehouses:
+            amount = warehouse.get_amount(order.sku)
+            if order.amount <= amount:
+                break
+        if order.amount <= amount:
+            warehouse.decrement(order.sku, order.amount)
+            order.warehouses[warehouse.get_name()] = order.amount
+            order.shipped = True
+            order.tracking_number = tracking_number
+            return order.shipped
+
+        # Divide order for few warehouses
+        order_amount = order.amount
+        for warehouse in self.warehouses:
+            amount = warehouse.get_amount(order.sku)
+            if order_amount <= amount:
+                warehouse.decrement(order.sku, order_amount)
+                order.warehouses[warehouse.get_name()] = order_amount
+                order.shipped = True
+                order.tracking_number = tracking_number
+                return order.shipped
+            else:
+                warehouse.decrement(order.sku, amount)
+                order.warehouses[warehouse.get_name()] = amount
+                order_amount -= amount
+
+        self.cancel_order(order)
+        return False
         
     def cancel_order(self, order):
         """
@@ -107,9 +142,12 @@ class OrderManager(object):
         the order.
         :param order: Order to Cancel
         :return: True if canceled False otherwise.
-
-        Perform any inventory management you need to do here.
         """
+        for warehouse in self.warehouses:
+            warehouse_name = warehouse.get_name()
+            if warehouse_name in order.warehouses:
+                warehouse.increment(order.sku, order.warehouses[warehouse_name])
+                del order.warehouses[warehouse_name]
         order.canceled = True
         return order.canceled
 
@@ -119,9 +157,20 @@ if __name__ == '__main__':
     wareHouseCalifornia.increment('APP', 500)
     wareHouseCalifornia.increment('ORG', 200)
 
-    wareHouseNewYork = WareHouse('NewYork')
+    wareHouseNewYork = WareHouse('New York')
     wareHouseNewYork.increment('APP', 1200)
     wareHouseNewYork.increment('ORG', 700)
 
     orderManager = OrderManager([wareHouseCalifornia, wareHouseNewYork])
-    pass
+
+    print('Test race conditions:')
+    order1 = Order('ORG', 600)
+    order2 = Order('ORG', 500)
+
+    if orderManager.acknowledge_order(order1) and orderManager.acknowledge_order(order2):
+        orderManager.ship_order(order1, 'track111')
+        # Race condition: order acknowledged, but can not be shipped
+        orderManager.ship_order(order2, 'track222')
+
+    print(order1)
+    print(order2)
